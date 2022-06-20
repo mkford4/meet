c167c050e323f080c1631b2d059c05e803b629b9
Monika <91907563+mkford4@users.noreply.github.com>
2022-06-19 19:36:26 -0500

    Added styling to App & WelcomeScreen. Created pie chart.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import { OfflineAlert } from './Alert';
 import {
   ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
-
+import EventGenre from './EventGenre';
+import { Col, Container, Row, Navbar } from 'react-bootstrap';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32,
+    numberOfEvents: 32, //default value
     showWelcomeScreen: undefined,
   }
 
@@ -44,7 +45,7 @@ class App extends Component {
     }
 
     this.setState({ showWelcomeScreen: !(code || isTokenValid) || this.props.isTestMode, offlineText });
-
+    window.addEventListener('offline', getEvents());
   }
 
 
@@ -90,33 +91,58 @@ class App extends Component {
   }
 
   render() {
-    //if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
 
     const { events, locations, updateEvents } = this.state;
 
     return (
       <div className="App">
-        <OfflineAlert text={this.state.offlineText} />
-        <h1>Meet App</h1>
-        <h4>Search your city here:</h4>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <br />
-        <h4>Number of Events:</h4>
-        <NumberOfEvents updateEvents={this.updateEvents} events={this.state.events} />
+        <Navbar fixed='top' bg='light' expand='lg'>
+          <Container className='navbar-brand-wrapper'>
+            <ul class='navbar-nav mx-auto'>
+              <li>
+                <a className='navbar-brand'>Meet App</a>
+              </li>
+            </ul>
+          </Container>
+        </Navbar>
+
+        <div className="search-wrapper">
+          <h4>Search your city here:</h4>
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
+          />
+          <br />
+          <h3>Number of events:</h3>
+          <NumberOfEvents
+            updateEvents={this.updateEvents}
+            events={this.state.events}
+          />
+        </div>
+        <div className='data-vis-wrapper'>
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid />
+              <XAxis dataKey="city" name="city" type="category" />
+              <YAxis dataKey="number" name="number of events" type="number" allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#f28444" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <h4 className='upcoming-events'>Upcoming Events:</h4>
-        <ResponsiveContainer height={400}>
-          <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid />
-            <XAxis dataKey="city" name="city" type="category" />
-            <YAxis dataKey="number" name="number of events" type="number" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
         <EventList events={this.state.events} />
 
-
+        <div className="alert">
+          {!navigator.onLine ? (
+            <OfflineAlert text={this.state.offlineText} />
+          ) : ('')}
+        </div>
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={getAccessToken} />
       </div>
+
     );
   }
 }
--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 import './WelcomeScreen.css';
 
+
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
     (
       <div className='WelcomeScreen'>
-        <h1>Welcome to the Meet App</h1>
-        <h4>
-          Log in to see upcoming events around the world for full-stack developers
-        </h4>
-        <div className='button_cont' alight='center'>
-          <div className='google-btn'>
+        <div className='welcome-wrapper'>
+          <h1>WELCOME to the MEET APP</h1>
+          <h4>
+            Log in to see upcoming events around the world for full-stack developers
+          </h4>
+
+
+          <button className='google-btn' onClick={() => { props.getAccessToken() }}
+            rel='nofolow noopener'>
+            <b className='btn-text'>Sign in with Google:</b>
             <div className='google-icon-wrapper'>
               <img
-                className='google-icon'
-                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-                      o.svg'
+
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
                 alt='Google sign-in'
               />
             </div>
-            <button onClick={() => { props.getAccessToken() }}
-              rel='nofolow noopener'
-              className='btn-text' >
-              <b>Sign in with Google:</b>
-            </button>
-          </div>
+
+          </button>
+
         </div>
         <a
           href='https://mkford4.github.io/meet/privacy.html'
           rel='nofollow noopener'
+          className='footer'
         >
           Privacy Policy
         </a>
